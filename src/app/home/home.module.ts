fdc1c5e460e74c9e571032106ccb409db1b2f95b
Matheus eli <matheuselisouzaferreira@hotmail.com>
2021-12-06 22:48:41 -0300

    header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HeaderComponent } from '../shared/component/header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
